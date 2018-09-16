0652ddc62e5408885bc87b49675261a6e3cb5855
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-16 17:49:33 +0200

    Add content for the 'Watches' category

---
+++ src/js/database/accessories/watches.js
@@ -0,0 +1,59 @@
+const watches = [
+  {
+    brand: "Fossil",
+    title: "RILEY - Zegarek - gold",
+    price: 539.0,
+    photosPath: "riley"
+  },
+
+  {
+    brand: "Diesel",
+    title: "MD RD GUN BLK ST - Zegarek",
+    price: 1389.0,
+    photosPath: "md-rd-gun"
+  },
+
+  {
+    brand: "Swatch",
+    title: "SKINGALA S - Zegarek",
+    price: 505.0,
+    photosPath: "skingala-s"
+  },
+
+  {
+    brand: "Swarovski",
+    title: "LOVELY MINI - Zegarek",
+    price: 1789.0,
+    photosPath: "lovely-mini"
+  },
+
+  {
+    brand: "KIOMI",
+    title: "Zegarek",
+    price: 84.0,
+    photosPath: "kiomi"
+  },
+
+  {
+    brand: "Anna Field",
+    title: "Zegarek",
+    price: 69.0,
+    photosPath: "anna-field"
+  },
+
+  {
+    brand: "Even&Odd",
+    title: "Zegarek - rosegold",
+    price: 69.0,
+    photosPath: "even-and-odd"
+  },
+
+  {
+    brand: "DKNY",
+    title: "SOHO - Zegarek - roségold",
+    price: 548.1,
+    photosPath: "dkny"
+  }
+];
+
+export { watches };
