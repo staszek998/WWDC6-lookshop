8a63b6f41c23ea8387da84a88f8636ef990b8165
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-16 17:12:30 +0200

    Add content for the 'High heels' category

--- src/js/database/shoes/high-heels.js
+++ src/js/database/shoes/high-heels.js
@@ -1,9 +1,58 @@
 const highHeels = [
   {
-    brand: "Nike Sportswear",
-    title: "INTERNATIONALIST - Sneakersy niskie",
-    price: 379.0,
-    photosPath: "internationalist"
+    brand: "MAI PIÙ SENZA",
+    title: "Sandały na obcasie",
+    price: 341.1,
+    photosPath: "mai-piu-senza"
+  },
+
+  {
+    brand: "ALDO",
+    title: "CASSEDY - Szpilki",
+    price: 349.0,
+    photosPath: "cassedy"
+  },
+
+  {
+    brand: "Stuart Weitzman",
+    title: "NUDISTSONG - Sandały na obcasie",
+    price: 1809.0,
+    photosPath: "nudistsong"
+  },
+
+  {
+    brand: "Högl",
+    title: "Szpilki",
+    price: 649.0,
+    photosPath: "hogl"
+  },
+
+  {
+    brand: "Tamaris",
+    title: "Szpilki",
+    price: 161.1,
+    photosPath: "tamaris"
+  },
+
+  {
+    brand: "Peter Kaiser",
+    title: "HERDI - Szpilki",
+    price: 699.0,
+    photosPath: "herdi"
+  },
+
+  {
+    brand: "Zign",
+    title: "Szpilki",
+    price: 271.2,
+    photosPath: "zign"
+  },
+
+  {
+    brand: "Kurt Geiger",
+    title: "BOND - Szpilki",
+    price: 719.0,
+    photosPath: "kurt-geiger"
   }
 ];
 
