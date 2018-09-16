7778627dd00980e44290eb430722816abfd3c3aa
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-16 09:12:21 +0200

    Add first element to the database

---
+++ src/js/database.js
@@ -0,0 +1,12 @@
+const database = {
+    underwear: {
+        tightsAndStockings: [
+            {
+                brand: 'Falke',
+                title: 'Zakolanówki',
+                price: 109.99,
+                photosPath: 'falkeStockings'
+            }
+        ]
+    }
+}
