96d481f952b70629086c7463c8b31a82d98aa056
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-16 15:49:55 +0200

    Add content for the 'Tops and T-shirts' category

--- src/js/clothes/dresses.js
+++ src/js/clothes/dresses.js
@@ -5,49 +5,42 @@ const dresses = [
     price: 399.0,
     photosPath: "king-louie"
   },
-
   {
     brand: "Zalando Essentials",
     title: "Sukienka z dżerseju",
     price: 119.0,
     photosPath: "zalando-essentials"
   },
-
   {
     brand: "Lee",
     title: "DRESS - Sukienka letnia",
     price: 249.0,
     photosPath: "lee"
   },
-
   {
     brand: "Noisy May",
     title: "NMCIRUS FUNNEL NECK DRESS - Sukienka etui",
     price: 209.0,
     photosPath: "noisy-may"
   },
-
   {
     brand: "Vila",
     title: "VILUCY - Sukienka letnia",
     price: 209.0,
     photosPath: "vilucy"
   },
-
   {
     brand: "Zalando Essentials",
     title: "Długa sukienka - navy/off",
     price: 99.0,
     photosPath: "zalando-essentials-navy"
   },
-
   {
     brand: "Warehouse",
     title: "PUFF SLEEVE CREPE DRESS - Sukienka letnia",
     price: 239.0,
     photosPath: "warehouse"
   },
-
   {
     brand: "Vila",
     title: "VIFELLOW DRESS - Sukienka z dżerseju",
---
+++ src/js/clothes/tops-and-t-shirts.js
@@ -0,0 +1,59 @@
+const topsAndTShirts = [
+  {
+    brand: "mbyM",
+    title: "LIONE - Bluzka z długim rękawem",
+    price: 209.0,
+    photosPath: "mbym"
+  },
+
+  {
+    brand: "Object",
+    title: "OBJLEENA SINGLET - Top",
+    price: 89.0,
+    photosPath: "object"
+  },
+
+  {
+    brand: "Vero Moda",
+    title: "VMMAXI - Top",
+    price: 59.0,
+    photosPath: "vmmaxi"
+  },
+
+  {
+    brand: "Selected Femme",
+    title: "SFMIO NOOS - Bluzka z długim rękawem",
+    price: 119.0,
+    photosPath: "sfmio"
+  },
+
+  {
+    brand: "Zalando Essentials",
+    title: "T-shirt basic",
+    price: 54.0,
+    photosPath: "zalando-essentials"
+  },
+
+  {
+    brand: "Zalando Essentials",
+    title: "2 PACK - Bluzka z długim rękawem",
+    price: 84.0,
+    photosPath: "zalando-essentials-2-pack"
+  },
+
+  {
+    brand: "Levi's",
+    title: "THE PERFECT - T-shirt z nadrukiem",
+    price: 79.0,
+    photosPath: "levis"
+  },
+
+  {
+    brand: "Tommy Hilfiger",
+    title: "TOMMY ICONS - T-shirt z nadrukiem",
+    price: 199.0,
+    photosPath: "tommy-icons"
+  }
+];
+
+export { topsAndTShirts };
