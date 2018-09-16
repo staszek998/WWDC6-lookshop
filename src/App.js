d0d86de4fe667fb55a454817d943169e41da1e71
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-16 18:29:33 +0200

    Create the Main.jsx file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Main from "./js/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Main />
       </div>
     );
   }
