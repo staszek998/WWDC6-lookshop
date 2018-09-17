255b42d04db1bd5f673d428116f87a1028c7dde5
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-09-17 16:38:08 +0200

    Merge 'design-the-grid' into 'master' Squashed commit of the following:
    commit 636c09feb2912ec53887f4c945c0badc08def503
    Merge: bcf1d87 b41cd0c
    Author: Stanisław Gregor <stanislaw.gregor@plej.pl>
    Date:   Mon Sep 17 16:37:28 2018 +0200
    
        Merge 'master' into 'design-the-grid'
    
    commit bcf1d87703d4c4ad050a1d0cb065161a9bd726af
    Author: Stanisław Gregor <stanislaw.gregor@plej.pl>
    Date:   Mon Sep 17 16:05:10 2018 +0200
    
        Start writing the 'Header' and its subcomponents
    
    commit b8b79432426bdbcbe2695e4498e1b3506a6892da
    Author: Stanisław Gregor <stanislaw.gregor@plej.pl>
    Date:   Mon Sep 17 15:07:38 2018 +0200
    
        Create the 'MyAccount' component
    
    commit 8cfb9e670783b2bb0cc318d9312dae468232d59a
    Author: Stanisław Gregor <stanislaw.gregor@plej.pl>
    Date:   Mon Sep 17 15:05:51 2018 +0200
    
        Create the 'CurrencySelector' component
    
    commit 3b0f3ea70cd5711c4bb607430d45b298df194ea9
    Author: Stanisław Gregor <stanislaw.gregor@plej.pl>
    Date:   Mon Sep 17 15:01:32 2018 +0200
    
        Create 'CustomerCare' component

--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import CustomerCare from "./components/Header/CustomerCare/CustomerCare";
+import CurrencySelector from "./components/Header/CurrencySelector/CurrencySelector";
+import MyAccount from "./components/Header/MyAccount/MyAccount";
+import Search from "./components/Header/Search/Search";
+
 class Main extends Component {
   render() {
     return (
---
+++ src/js/components/Header/MyAccount/MyAccount.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MyAccount extends Component {
+  render() {
+    return (
+      <div className="cell expand medium-shrink">
+        <a href="#">MY ACCOUNT</a>
+      </div>
+    );
+  }
+}
+
+export default MyAccount;
