5250f9d77adb3d027de2e5df2b3b5ac8982a3e26
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-09-17 17:55:37 +0200

    Code the row for the favourite brands section

--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -115,6 +115,30 @@ class Main extends Component {
               </div>
             </div>
           </div>
+
+          {/* FAVOURITE BRANDS */}
+          <div className="row border d-none d-md-block">
+            <div className="col text-center border">
+              favourite brands (top) separator
+            </div>
+          </div>
+          <div className="container">
+            <div className="row border d-none d-md-flex">
+              <div className="col border d-none d-lg-block">brand</div>
+              <div className="col border d-none d-lg-block">brand</div>
+              <div className="col border">brand</div>
+              <div className="col border">brand</div>
+              <div className="col border">brand</div>
+              <div className="col border">brand</div>
+              <div className="col border">brand</div>
+              <div className="col border">brand</div>
+            </div>
+          </div>
+          <div className="row border d-none d-md-block">
+            <div className="col text-center border">
+              favourite brands (bottom) separator
+            </div>
+          </div>
         </main>
       </Router>
     );
