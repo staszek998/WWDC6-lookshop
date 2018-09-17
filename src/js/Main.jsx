42ef909ba6624b6e81dfa50b63fcb7cce448c407
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-09-17 09:35:28 +0200

    Remove all referrences to the Foundation

--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Home from "./tmp-router-paths/Home";
-import About from "./tmp-router-paths/About";
-import Contact from "./tmp-router-paths/Contact";
-
 class Main extends Component {
-  goBack = () => {};
-
   render() {
     return (
       <Router>
