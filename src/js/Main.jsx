f0a7523d2591312511e03be7bac954c5f87b253a
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-09-17 17:19:25 +0200

    Add borders for easier elements recognising

--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -14,31 +14,33 @@ class Main extends Component {
           <div className="container">
             {/* HEADER */}
             <div className="row justify-content-between border">
-              <div className="col-12 col-md-6">CUSTOMER CARE</div>
-              <div className="col-12 col-md-6">
-                <div className="row justify-content-between justify-content-md-end">
-                  <div className="col-3 col-md-auto">CURRENCY $</div>
-                  <div className="col-6 col-md-auto">MY ACCOUNT</div>
-                  <div className="col-3 col-md-auto">Search</div>
+              <div className="col-12 col-md-6 border">CUSTOMER CARE</div>
+              <div className="col-12 col-md-6 border">
+                <div className="row justify-content-between justify-content-md-end border">
+                  <div className="col-3 col-md-auto border">CURRENCY $</div>
+                  <div className="col-6 col-md-auto border">MY ACCOUNT</div>
+                  <div className="col-3 col-md-auto border">Search</div>
                 </div>
               </div>
             </div>
 
             {/* LOGO, MENU & CART */}
-            <div className="row">
-              <div className="col-12 col-md-8 col-lg-3 order-1">logo</div>
-              <div className="col-12 col-md-4 col-lg-3 order-2 order-lg-3">
+            <div className="row border">
+              <div className="col-12 col-md-8 col-lg-3 order-1 border">
+                logo
+              </div>
+              <div className="col-12 col-md-4 col-lg-3 order-2 order-lg-3 border">
                 cart (3rd on lg)
               </div>
-              <div className="col-12 col-lg-6 order-3 order-lg-2">
+              <div className="col-12 col-lg-6 order-3 order-lg-2 border">
                 menu (2nd on lg)
               </div>
             </div>
           </div>
 
           {/* SLIDER */}
-          <div className="jumbotron-fluid">
-            <div className="col-12 d-none d-md-flex">slider</div>
+          <div className="jumbotron-fluid border">
+            <div className="col-12 d-none d-md-flex border">slider</div>
           </div>
         </main>
       </Router>
