5d317eb7ada15ea1e2676f50ca50dbc51395c3c0
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-09-18 08:38:31 +0200

    Code the section for the tablet and desktop footer top

--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -168,27 +168,31 @@ class Main extends Component {
             <div className="col-auto border">accepted payments</div>
           </div>
 
-          {/* FOOTER */}
-          {/* 
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          */}
+          {/* FOOTER TOP (TABLET & DESKTOP) */}
+          <div className="row border d-none d-md-flex">
+            <div className="col border">
+              <div className="container border">
+                <div className="row border">
+                  <div className="col-6 col-lg-8 border">
+                    <div className="row border">
+                      <div className="col-6 col-lg-3 border">
+                        customer service
+                      </div>
+                      <div className="col-6 col-lg-3 border">information</div>
+                      <div className="col-6 col-lg-3 border">campaigns</div>
+                      <div className="col-6 col-lg-3 border">stores</div>
+                    </div>
+                  </div>
+                  <div className="col-6 col-lg-4 border">
+                    <div className="row border">
+                      <div className="col-6 border">social media links</div>
+                      <div className="col-6 border">100% secured badge</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </Router>
     );
