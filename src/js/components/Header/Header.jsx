7e8e4b257571bb60c5e2fb52711627fdaa78e785
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-23 09:39:00 +0200

    Add currency selector

--- src/js/components/Header/Header.jsx
+++ src/js/components/Header/Header.jsx
@@ -7,12 +7,20 @@ class Header extends Component {
         <div className="container">
           <div className="row justify-content-between">
             <div className="col-12 col-md-6 header__left-column">
-              CUSTOMER CARE: +22 654 25 98
+              <span>CUSTOMER CARE: +22 654 25 98</span>
             </div>
             <div className="col-12 col-md-6 header__right-column">
-              <div className="row justify-content-between justify-content-md-end">
+              <div className="row justify-content-between justify-content-md-end h-100">
                 <div className="col-3 col-md-auto border-left border-right">
-                  CURRENCY $
+                  <select
+                    name="currency"
+                    id="currency"
+                    className="custom-select bg-transparent border-0 text-white"
+                  >
+                    <option value="usd">$ USD</option>
+                    <option value="eur">€ EUR</option>
+                    <option value="pln">PLN</option>
+                  </select>
                 </div>
                 <div className="col-6 col-md-auto border-right">MY ACCOUNT</div>
                 <div className="col-3 col-md-auto border-right">Search</div>
