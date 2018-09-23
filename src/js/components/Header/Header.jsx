5fc9b8b8827a0abf85c16bf077617eb07668bc0a
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-23 09:26:50 +0200

    Add media query for handling the background color change

--- src/js/components/Header/Header.jsx
+++ src/js/components/Header/Header.jsx
@@ -6,10 +6,14 @@ class Header extends Component {
       <div className="row header">
         <div className="container">
           <div className="row justify-content-between">
-            <div className="col-12 col-md-6">CUSTOMER CARE: +22 654 25 98</div>
-            <div className="col-12 col-md-6">
+            <div className="col-12 col-md-6 header__left-column">
+              CUSTOMER CARE: +22 654 25 98
+            </div>
+            <div className="col-12 col-md-6 header__right-column">
               <div className="row justify-content-between justify-content-md-end">
-                <div className="col-3 col-md-auto border-left border-right">CURRENCY $</div>
+                <div className="col-3 col-md-auto border-left border-right">
+                  CURRENCY $
+                </div>
                 <div className="col-6 col-md-auto border-right">MY ACCOUNT</div>
                 <div className="col-3 col-md-auto border-right">Search</div>
               </div>
