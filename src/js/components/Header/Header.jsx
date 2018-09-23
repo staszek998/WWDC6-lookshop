b49f51b9dbfe22890b592c157fab52bc078a9208
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-23 08:32:28 +0200

    Remove borders from the Header

--- src/js/components/Header/Header.jsx
+++ src/js/components/Header/Header.jsx
@@ -3,15 +3,15 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div className="row border header">
+      <div className="row header">
         <div className="container">
-          <div className="row justify-content-between border">
-            <div className="col-12 col-md-6 border">CUSTOMER CARE</div>
-            <div className="col-12 col-md-6 border">
-              <div className="row justify-content-between justify-content-md-end border">
-                <div className="col-3 col-md-auto border">CURRENCY $</div>
-                <div className="col-6 col-md-auto border">MY ACCOUNT</div>
-                <div className="col-3 col-md-auto border">Search</div>
+          <div className="row justify-content-between">
+            <div className="col-12 col-md-6">CUSTOMER CARE</div>
+            <div className="col-12 col-md-6">
+              <div className="row justify-content-between justify-content-md-end">
+                <div className="col-3 col-md-auto">CURRENCY $</div>
+                <div className="col-6 col-md-auto">MY ACCOUNT</div>
+                <div className="col-3 col-md-auto">Search</div>
               </div>
             </div>
           </div>
