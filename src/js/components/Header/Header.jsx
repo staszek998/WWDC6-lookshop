edd3b5b331888dc951c17584e709e1b4dc162661
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-23 14:31:34 +0200

    Style the custom-select

--- src/js/components/Header/Header.jsx
+++ src/js/components/Header/Header.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
       <div className="row header">
         <div className="container">
           <div className="row justify-content-between">
-            <div className="col-12 col-md-6 header__left-column">
+            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center justify-content-md-start header__left-column">
               <span>CUSTOMER CARE: +22 654 25 98</span>
             </div>
             <div className="col-12 col-md-6 header__right-column">
@@ -15,15 +15,19 @@ class Header extends Component {
                   <select
                     name="currency"
                     id="currency"
-                    className="custom-select bg-transparent border-0 text-white w-100"
+                    className="custom-select border-0 text-white w-100 bg-transparent"
                   >
                     <option value="usd">$ USD</option>
                     <option value="eur">€ EUR</option>
                     <option value="pln">PLN</option>
                   </select>
                 </div>
-                <div className="col-6 col-md-auto border-right">MY ACCOUNT</div>
-                <div className="col-3 col-md-auto border-right">Search</div>
+                <div className="col-6 col-md-auto d-flex justify-content-center align-items-center border-right">
+                  MY ACCOUNT
+                </div>
+                <div className="col-3 col-md-auto d-flex justify-content-center align-items-center border-right">
+                  Search
+                </div>
               </div>
             </div>
           </div>
