b621ec96f4c2dbe83ca999774b31ab2c7ba939ff
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-29 21:17:39 +0200

    Remove unnecessary borders. This time with reflection to the important ones ;)

--- src/js/components/NewsletterAndSocialMedia/NewsletterAndSocialMedia.jsx
+++ src/js/components/NewsletterAndSocialMedia/NewsletterAndSocialMedia.jsx
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 class NewsletterAndSocialMedia extends Component {
   render() {
     return (
-      <div className="row border">
-        <div className="container border">
-          <div className="row border">
-            <div className="col-12 col-md-6 border">
+      <div className="row ">
+        <div className="container ">
+          <div className="row ">
+            <div className="col-12 col-md-6 ">
               newsletter subscription
             </div>
-            <div className="col-12 col-md-6 border">social media icons</div>
+            <div className="col-12 col-md-6 ">social media icons</div>
           </div>
         </div>
       </div>
