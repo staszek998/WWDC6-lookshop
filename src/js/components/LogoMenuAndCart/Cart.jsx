55417842bcba82422effb705a6aafce6fe2ba06e
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-29 22:42:50 +0200

    Create basic Cart layout

---
+++ src/js/components/LogoMenuAndCart/Cart.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col">{/* Dropdown */}</div>
+        </div>
+        <div className="row">
+          <div className="col">{/* Cart icon */}</div>
+          <div className="col">{/* Sum of cart items and price */}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cart;
