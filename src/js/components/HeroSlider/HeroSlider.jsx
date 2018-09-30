20abad1f3483fc1f9c653de9a63a2e18b7912e6d
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-09-30 13:28:51 +0200

    Copy/paste the slider component from the Bootstrap documentation

--- src/js/components/HeroSlider/HeroSlider.jsx
+++ src/js/components/HeroSlider/HeroSlider.jsx
@@ -3,8 +3,65 @@ import React, { Component } from "react";
 class HeroSlider extends Component {
   render() {
     return (
-      <div className="row d-none d-md-flex ">
-        <div className="col-12 ">hero slider</div>
+      <div className="row d-none d-md-flex hero-slider border">
+        <div className="col-12 ">
+          <div
+            id="carouselExampleIndicators"
+            className="carousel slide"
+            data-ride="carousel"
+          >
+            <ol className="carousel-indicators">
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="0"
+                className="active"
+              />
+              <li data-target="#carouselExampleIndicators" data-slide-to="1" />
+              <li data-target="#carouselExampleIndicators" data-slide-to="2" />
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  className="d-block w-100"
+                  src=".../800x400?auto=yes&bg=777&fg=555&text=First slide"
+                  alt="First slide"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide"
+                  alt="Second slide"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide"
+                  alt="Third slide"
+                />
+              </div>
+            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselExampleIndicators"
+              role="button"
+              data-slide="prev"
+            >
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="sr-only">Previous</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselExampleIndicators"
+              role="button"
+              data-slide="next"
+            >
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
