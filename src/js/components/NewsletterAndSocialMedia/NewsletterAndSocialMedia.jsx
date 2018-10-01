1e6ca45104e73b310ae066dc90cbe58917e8a30a
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-10-01 13:37:30 +0200

    Add border to the main row

--- src/js/components/NewsletterAndSocialMedia/NewsletterAndSocialMedia.jsx
+++ src/js/components/NewsletterAndSocialMedia/NewsletterAndSocialMedia.jsx
@@ -3,12 +3,10 @@ import React, { Component } from "react";
 class NewsletterAndSocialMedia extends Component {
   render() {
     return (
-      <div className="row ">
+      <div className="row border">
         <div className="container ">
           <div className="row ">
-            <div className="col-12 col-md-6 ">
-              newsletter subscription
-            </div>
+            <div className="col-12 col-md-6 ">newsletter subscription</div>
             <div className="col-12 col-md-6 ">social media icons</div>
           </div>
         </div>
