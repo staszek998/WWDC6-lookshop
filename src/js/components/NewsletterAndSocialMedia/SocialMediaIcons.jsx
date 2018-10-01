f31e22d97ff96fd42b965b6f45385dd53de8778a
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-10-01 15:47:41 +0200

    Leaving work

--- src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
+++ src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
@@ -3,16 +3,19 @@ import React, { Component } from "react";
 class SocialMediaIcons extends Component {
   render() {
     return (
-      <div className="col-12 col-md-6">
-        <div className="row justify-content-center justify-content-md-end align-items-center mb-3">
+      <div className="col-12 col-md-6 social-media-icons">
+        <div className="row justify-content-center justify-content-md-end align-items-center mb-3 h-100">
           <span className="text-secondary mr-3">Find us here</span>
-          <button className="btn rounded-0 btn-outline-secondary">
-            <i className="fab fa-facebook-f" />
-          </button>
-          <button className="btn rounded-0 btn-outline-secondary">
+
+            <button className="btn rounded-0">
+              <i className="fab fa-facebook-f" />
+            </button>
+
+          <button className="btn rounded-0">
             <i className="fab fa-twitter" />
           </button>
-          <button className="btn rounded-0 btn-outline-secondary">
+
+          <button className="btn rounded-0">
             <i className="fab fa-pinterest" />
           </button>
         </div>
