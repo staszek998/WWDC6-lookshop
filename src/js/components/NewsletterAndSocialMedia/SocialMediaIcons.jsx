05591fd5e3d3cb192d884249fcae5f3724a3a0c1
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-10-01 14:49:01 +0200

    Correct the background color change

--- src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
+++ src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
@@ -4,10 +4,18 @@ class SocialMediaIcons extends Component {
   render() {
     return (
       <div className="col-12 col-md-6">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        neque quisquam possimus, molestias cumque ad temporibus fugit ipsam
-        alias nostrum voluptatum aperiam dignissimos? Animi, deserunt corporis
-        magni provident asperiores autem?
+        <div className="row justify-content-center justify-content-md-end align-items-center mb-3">
+          <span className="text-secondary mr-3">Find us here</span>
+          <button className="btn rounded-0 btn-outline-secondary">
+            <i className="fab fa-facebook-f" />
+          </button>
+          <button className="btn rounded-0 btn-outline-secondary">
+            <i className="fab fa-twitter" />
+          </button>
+          <button className="btn rounded-0 btn-outline-secondary">
+            <i className="fab fa-pinterest" />
+          </button>
+        </div>
       </div>
     );
   }
