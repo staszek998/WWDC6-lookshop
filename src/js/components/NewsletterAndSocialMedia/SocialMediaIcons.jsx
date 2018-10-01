777198bd2a8874c4f3a03b6aecee782f445092ee
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-10-01 14:34:29 +0200

    Create the subcomponent

---
+++ src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class SocialMediaIcons extends Component {
+  render() {
+    return (
+      <div className="col-12 col-md-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        neque quisquam possimus, molestias cumque ad temporibus fugit ipsam
+        alias nostrum voluptatum aperiam dignissimos? Animi, deserunt corporis
+        magni provident asperiores autem?
+      </div>
+    );
+  }
+}
+
+export default SocialMediaIcons;
