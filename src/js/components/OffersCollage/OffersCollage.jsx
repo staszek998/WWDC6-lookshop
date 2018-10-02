5e2f30d6eee4e2aaa204331e735ba22b416026bb
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-10-02 09:37:08 +0200

    Remove empty spaces

--- src/js/components/OffersCollage/OffersCollage.jsx
+++ src/js/components/OffersCollage/OffersCollage.jsx
@@ -3,16 +3,16 @@ import React, { Component } from "react";
 class OffersCollage extends Component {
   render() {
     return (
-      <div className="container  d-none d-md-block ">
-        <div className="row ">
-          <div className="col ">offer</div>
-          <div className="col ">
-            <div className="row ">
-              <div className="col ">offer</div>
+      <div className="container d-none d-md-block">
+        <div className="row">
+          <div className="col">offer</div>
+          <div className="col">
+            <div className="row">
+              <div className="col">offer</div>
             </div>
-            <div className="row ">
-              <div className="col ">offer</div>
-              <div className="col ">offer</div>
+            <div className="row">
+              <div className="col">offer</div>
+              <div className="col">offer</div>
             </div>
           </div>
         </div>
