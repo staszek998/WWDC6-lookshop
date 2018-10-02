2b72e09ee80d03837a5acdf17b9fc8e356446db8
Stanisław Gregor <stanislaw.gregor@plej.pl>
2018-10-02 09:29:32 +0200

    Handle the buttons dimensions

--- src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
+++ src/js/components/NewsletterAndSocialMedia/SocialMediaIcons.jsx
@@ -7,15 +7,15 @@ class SocialMediaIcons extends Component {
         <div className="row justify-content-center justify-content-md-end align-items-center mb-3 h-100">
           <span className="text-secondary mr-3">Find us here</span>
 
-            <button className="btn rounded-0">
-              <i className="fab fa-facebook-f" />
-            </button>
+          <button className="btn rounded-0 facebook">
+            <i className="fab fa-facebook-f" />
+          </button>
 
-          <button className="btn rounded-0">
+          <button className="btn rounded-0 twitter">
             <i className="fab fa-twitter" />
           </button>
 
-          <button className="btn rounded-0">
+          <button className="btn rounded-0 pinterest">
             <i className="fab fa-pinterest" />
           </button>
         </div>
