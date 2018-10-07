dfd40ac87dab582b7a51258f081b6d5c40aabbb4
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 17:01:33 +0200

    Create the function 'renderProducts'

--- src/js/components/SelectOrTabs/Mobile/Products/Products.jsx
+++ src/js/components/SelectOrTabs/Mobile/Products/Products.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 
 import Product from "./Product/Product";
-import { dresses } from "../../../../database/clothes/dresses";
+import { database } from "../../../../database/database";
+
+const whatsHot = `WHAT'S HOT?`;
 
 class Products extends Component {
   state = {
     collapsed: true
+  };
+
+  renderProducts = productsToRender => {
+    return (
+      <div className="row products">
+        {/* Products shown by default */}
+        <Product productObject={productsToRender[0]} />
+        <Product productObject={productsToRender[1]} />
+
+        {/* Products initially collapsed */}
+        <div className="col-12 collapse" id="hiddenProducts">
+          <div className="row">
+            {productsToRender.map((product, index) => {
+              if (index < 2) {
+                return null;
+              } else {
+                return <Product productObject={product} />;
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    );
   };
 
   buttonClickHandler = () => {
