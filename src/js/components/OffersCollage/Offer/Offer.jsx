889ce28949ae8c81dd72826eb82ebe68702cc983
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 10:09:14 +0200

    Offers' images are now links

--- src/js/components/OffersCollage/Offer/Offer.jsx
+++ src/js/components/OffersCollage/Offer/Offer.jsx
@@ -4,11 +4,13 @@ class Offer extends Component {
   render() {
     return (
       <div className="offer w-100 h-100">
-        <img
-          src={this.props.imgSrc}
-          alt={this.props.imgAlt}
-          className="offer-image"
-        />
+        <a href="#" className="offer-link">
+          <img
+            src={this.props.imgSrc}
+            alt={this.props.imgAlt}
+            className="offer-image"
+          />
+        </a>
       </div>
     );
   }
