360fe303270172893f58277df0eb14e784a94a79
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 10:48:19 +0200

    Add background color to the main rows

--- src/js/components/SelectOrTabs/Mobile/Mobile.jsx
+++ src/js/components/SelectOrTabs/Mobile/Mobile.jsx
@@ -4,10 +4,10 @@ class Mobile extends Component {
   render() {
     return (
       <div className="container mobile">
-        <div className="row">
+        <div className="row select">
           <div className="col">select</div>
         </div>
-        <div className="row">
+        <div className="row products">
           <div className="col">products</div>
         </div>
       </div>
