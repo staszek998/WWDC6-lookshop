76635d0d4d7dd67fb0b9d575cd2223858a170e77
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 10:32:27 +0200

    Create the Mobile.jsx subcomponent

---
+++ src/js/components/SelectOrTabs/Mobile/Mobile.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Mobile extends Component {
+  render() {
+    return <div className="mobile">mobile</div>;
+  }
+}
+
+export default Mobile;
--- src/js/components/SelectOrTabs/SelectOrTabs.jsx
+++ src/js/components/SelectOrTabs/SelectOrTabs.jsx
@@ -5,20 +5,20 @@ class SelectOrTabs extends Component {
     return (
       <div>
         <div className="row d-md-none">
-          <div className="container ">
-            <div className="row ">
-              <div className="col ">SELECT on mobile</div>
+          <div className="container">
+            <div className="row">
+              <div className="col">SELECT on mobile</div>
             </div>
           </div>
         </div>
 
-        <div className="row  d-none d-md-flex justify-content-between">
-          <div className="col ">TABS on medium and above</div>
-          <div className="col ">arrows &lt; &gt;</div>
+        <div className="row d-none d-md-flex justify-content-between">
+          <div className="col">TABS on medium and above</div>
+          <div className="col">arrows &lt; &gt;</div>
         </div>
 
-        <div className="row ">
-          <div className="col ">offers slider</div>
+        <div className="row">
+          <div className="col">offers slider</div>
         </div>
       </div>
     );
