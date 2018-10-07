9d781a773f6fcc792040dad77f13a4d3f053c39b
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 15:42:30 +0200

    Create basic structure of the Product component

--- src/js/components/SelectOrTabs/Mobile/Products/Product/Product.jsx
+++ src/js/components/SelectOrTabs/Mobile/Products/Product/Product.jsx
@@ -2,7 +2,20 @@ import React, { Component } from "react";
 
 class Product extends Component {
   render() {
-    return <div className="product col-6">product</div>;
+    return (
+      <div className="product col-6">
+        <img src={this.props.imgSrc} alt={this.props.imgAlt} />
+        <div className="row">
+          <div className="col-12 text-center text-dark">
+            <span>CLOTHING TITLE</span>
+            <span>$99.99</span>
+            <button className="btn rounded-0">
+              ADD TO CART <i className="fas fa-plus" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
