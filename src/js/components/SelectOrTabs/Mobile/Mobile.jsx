7e7978d8beb5149bf24280b1b52d730a6d62f0a0
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 11:34:57 +0200

    Change dropdown to collapse

--- src/js/components/SelectOrTabs/Mobile/Mobile.jsx
+++ src/js/components/SelectOrTabs/Mobile/Mobile.jsx
@@ -4,36 +4,31 @@ class Mobile extends Component {
   render() {
     return (
       <div className="container mobile">
+        {/* Dropdown */}
         <div className="row select">
-          <div className="col">
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                id="dropdownMenuButton"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown button
-              </button>
-              <div
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton"
-              >
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
+          {/* Toggler button */}
+          <div className="col-12">
+            <a
+              className="btn btn-block text-white"
+              data-toggle="collapse"
+              href="#select-or-tabs__dropdown-content"
+              role="button"
+              aria-expanded="false"
+              aria-controls="select-or-tabs__dropdown-content"
+            >
+              Link with href
+            </a>
+          </div>
+
+          {/* Collapsed content */}
+          <div className="col-12">
+            <div className="collapse" id="select-or-tabs__dropdown-content">
+              {/* here goes my list */}
             </div>
           </div>
         </div>
+
+        {/* Products */}
         <div className="row products">
           <div className="col">products</div>
         </div>
