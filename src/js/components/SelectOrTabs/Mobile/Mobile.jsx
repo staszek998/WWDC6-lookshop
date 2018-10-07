ea5acb3c3b87db679ace6dbaa70fa60995a21ac0
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 10:49:26 +0200

    Add default bootstrap's dropdown

--- src/js/components/SelectOrTabs/Mobile/Mobile.jsx
+++ src/js/components/SelectOrTabs/Mobile/Mobile.jsx
@@ -5,7 +5,34 @@ class Mobile extends Component {
     return (
       <div className="container mobile">
         <div className="row select">
-          <div className="col">select</div>
+          <div className="col">
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown button
+              </button>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="row products">
           <div className="col">products</div>
