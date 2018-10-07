d0a476279ec771f823eedb65f0b2952d37b234c5
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-10-07 15:36:56 +0200

    Implement the show-collapse functionality

---
+++ src/js/components/SelectOrTabs/Mobile/Products/Product/Product.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  render() {
+    return <div className="product col-6">product</div>;
+  }
+}
+
+export default Product;
--- src/js/components/SelectOrTabs/Mobile/Products/Products.jsx
+++ src/js/components/SelectOrTabs/Mobile/Products/Products.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
+import Product from "./Product/Product";
 
 class Products extends Component {
   render() {
     return (
       <div className="row products">
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
-        <div className="col-6">content</div>
+        {/* Products shown by default */}
+        <Product />
+        <Product />
+
+        {/* Products initially collapsed */}
+        <div className="col-12 collapse" id="hiddenProducts">
+          <div className="row">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+          </div>
+        </div>
+
+        {/* Collapse toggler */}
+        <div className="col-12 text-center py-3">
+          <button
+            className="btn border-0 rounded-0 bg-dark text-white"
+            type="button"
+            data-toggle="collapse"
+            data-target="#hiddenProducts"
+            aria-expanded="false"
+            aria-controls="hiddenProducts"
+          >
+            LOAD MORE
+          </button>
+        </div>
       </div>
     );
   }
